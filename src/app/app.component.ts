934ca3ecf2dd81f0881a3f62161223d953960fb3
BasmaAltayeh <basma.altayeh@gmail.com>
2024-09-19 16:25:34 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLink} from '@angular/router';
+import { BookSearchComponent } from './features/explore/components/book-search/book-search.component';
+import { BookListComponent } from './features/explore/components/book-list/book-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink, RouterModule, CommonModule,BookSearchComponent,BookListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
